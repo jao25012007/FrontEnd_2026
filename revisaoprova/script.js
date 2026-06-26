4dfa8d2501abfc84433c5a4a1b0e1f432b50a94c
jao25012007 <jpzingra@gmail.com>
2026-06-26 10:46:23 -0300

    Add files via upload

---
+++ revisaoprova/script.js
@@ -0,0 +1,18 @@
+function limpar() {
+    document.getElementById("conteudo").innerHTML = "";
+}
+
+function mostrarTabela() {
+    document.getElementById("conteudo").innerHTML =
+        '<img src="Tabela_Jogos.png" alt="Tabela da Copa">';
+}
+
+function mostrarVinicius() {
+    document.getElementById("conteudo").innerHTML =
+        '<img src="_vinicius_junior.png" alt="Vinícius Júnior">';
+}
+
+function mostrarLogo() {
+    document.getElementById("conteudo").innerHTML =
+        '<img src="FIFA-World-Cup-26.jpg" alt="Logo da Copa">';
+}
